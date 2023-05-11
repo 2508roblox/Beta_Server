7ea0a9d2851beee862adc4c48f1b8b281b4944b5
2508roblox <trangiangzxc@gmail.com>
2023-05-11 16:22:58 +0700

    beta

---
+++ Router/PostRoute.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { CreatePost, GetAllUserPost, LikePost, Unlike, GetTimeline } from './../controller/PostController.js';
+const app = express();
+const router = express.Router();
+
+router.post('/', CreatePost)
+router.get('/:id/:page', GetAllUserPost)
+// router.get('/:id/timeline/', GetTimeline)
+router.get('/:id/timeline/:page', GetTimeline)
+//like
+router.put('/:id/like', LikePost)
+router.put('/:id/unlike', Unlike)
+export default router
---
+++ Schema/UserModel.js
@@ -0,0 +1,40 @@
+import mongoose, { model } from 'mongoose';
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    username: {
+        type: 'String',
+        required: true,
+
+    },
+    password: {
+        type: 'String',
+        required: true
+    },
+
+    firstname: {
+        type: 'String',
+        required: true
+    },
+    lastname: {
+        type: 'String',
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    email: 'String',
+    relationship: 'String',
+    address: 'String',
+    phone: 'String',
+    hometown: 'String',
+    coverimage: 'String',
+    avatar: 'String',
+    desc: 'String',
+    worksat: 'String',
+    skills: [],
+
+}, { timestamps: true });
+const UserModel = mongoose.model('User', UserSchema)
+export default UserModel
